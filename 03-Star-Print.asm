0991b77ae24b318c674ea4127891b58d6c3d7944
Arannamoy Mondal <arannamoymondal13@gmail.com>
2026-02-22 22:00:06 +0600

    upd

--- 03-Star-Print.asm
+++ 03-Star-Print.asm
@@ -1,58 +1,57 @@
-.model small                ; Use small memory model 
-.stack 100h                 ; Allocate 256 bytes for stack
+.model small  ; small memory model 
+.stack 100h   ; 256 bytes for stack
 
 .data
-    msg  db 'Enter number of rows: $'   ; Message to display 
+    msg0  db 'Input: $'   ; message to display 
 
 .code
 main proc
 
-    mov ax, @data           ; Load address of data segment into ax
-    mov ds, ax              ; Initialize ds with data segment address
+    mov ax, @data           ; load address of data segment into ax
+    mov ds, ax              ; load ax into ds
     
 
     mov ah, 9               ; print string
-    lea dx, msg             ; Load address of msg into dx
-    int 21h                 ; Calling interrupt to display msg
+    lea dx, msg0            ; msg0 load into dx
+    int 21h                 ; dos interrupt
 
-    mov ah, 1               ; Read single character
-    int 21h                 ; Wait for user input 
+    mov ah, 1               ; read single character
+    int 21h                 ; dos interrupt
 
-    sub al, 30h             ; Convert ASCII to number
-    mov cl, al              ; Store number of rows in cl
-
+    sub al, 30h             ; convert into ascii code
+    mov cl, al              ; load al to cl
     
-    mov dl, 10              ; Line Feed 
-    mov ah, 2               ; Print character
-    int 21h
-    mov dl, 13              ; Carriage Return 
+    mov dl, 10              ; line Feed 
+    mov ah, 2               ; print character
+    int 21h                 ; dos interrupt
+    mov dl, 13              ; carriage return 
     int 21h
 
 outer:                      
 
-    mov ch, 0               ; Clear ch register
-    mov ch, cl              ; Copy row count to ch 
+    mov ch, 0               ; clear ch register
+    mov ch, cl              ; copy row count to ch 
 
 print:                      
 
-    mov dl, '*'             ; Load '*' character into dl
-    mov ah, 2               ; Print character
-    int 21h                 ; Print '*'
+    mov dl, '*'             ; load * in dl
+    mov ah, 2               ; print character
+    int 21h                 ; print '*'
 
-    dec ch                  ; Decrease star counter
-    jnz print               ; If ch != 0, repeat printing stars
+    dec ch                  ; decrease star counter
+    jnz print               ; if not zero, repeat printing stars
 
-    mov dl, 10              ; Line Feed
-    mov ah, 2
+    mov dl, 10              ; line Feed
+    mov ah, 2               ; print single character
+    int 21h                 ; dos interrupt
+    mov dl, 13              ; carriage Return
     int 21h
-    mov dl, 13              ; Carriage Return
-    int 21h
 
-    dec cl                  ; Decrease row counter
-    jnz outer               ; If cl != 0, go back to outer loop
+    dec cl                  ; decrease row counter
+    jnz outer               ; if not zero , go back to outer loop
+    exit:                   ; label for terminate
+     mov ah, 4ch             ; terminate program
+     int 21h                 ; dos interrupt
 
-    mov ah, 4ch             ; Terminate program
-    int 21h                 
-
-main endp
-end main
+main endp ;end main procedure
+end main ; end main execution
