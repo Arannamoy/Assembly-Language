8a1dc793f9f3f7c64b18a068299b6f838792d9b6
Arannamoy Mondal <arannamoymondal13@gmail.com>
2026-02-22 21:25:18 +0600

    upd

--- 04-add.asm
+++ 04-add.asm
@@ -1,13 +1,14 @@
 .model small
 .stack 100h
+
 .data
-msg0 db 'Variables: num1=$'
-msg1 db ', num2=$'
-msg2 db 'Output: Sum=$'
+msg0 db 'Variables: num1 =$'
+msg1 db ',num2=$'
+msg2 db 'Output: Result =$'
 
 
 .code
-main proc
+main proc:
     mov ax,@data
     mov ds,ax
 
@@ -15,48 +16,4 @@ main proc
     lea dx,msg0
     int 21h
 
-    mov ah,1
-    int 21h
-    sub al,30h
-    mov bl,al
-
-    mov ah,9
-    lea dx,msg1
-    int 21h
-
-    mov ah,1
-    int 21h
-    sub al,30h
-    mov bh,al
-
-
-    mov al,bl
-    add al,bh
-
-    add al,30h
-
-    mov dl,al
-    mov ah,2
-    int 21h
-    ;mov dl,10
-    ;mov ah,2
-    ;int 21h
-
-    ;mov ah,9
-    ;lea dx,msg2
-    ;int 21h
-
-    ;mov dl,al
-    ;mov ah,2
-    ;int 21h
-    ;mov dl,13
-    ;int 21h
-
-    mov ah,4Ch
-    int 21h
-main endp
-end main
-
-
-
-
+    
--- test.asm
+++ test.asm
@@ -1,17 +1,53 @@
-.model small 
-.stack 100h
-.code
-main proc
-    mv ah,1
-    int 21h
-    mov bl,al
-    mov ah,2
-    mov dl,bl
-    int 21h
+; Q1: Star Pattern (Reverse Triangle)
+.MODEL SMALL
+.STACK 100H
+.DATA
+    PROMPT DB 'Enter number of rows: $'
+    NEWLINE DB 0DH, 0AH, '$'
+    ROWS DW ?
+.CODE
+MAIN PROC
+    MOV AX, @DATA
+    MOV DS, AX
 
-    exit:
-    mov ah,4ch
-    int 21h
-    main endp
+    ; Display Prompt [cite: 11]
+    MOV AH, 09H
+    LEA DX, PROMPT
+    INT 21H
 
-end main
+    ; Take Input (Single digit) [cite: 13]
+    MOV AH, 01H
+    INT 21H
+    SUB AL, 48      ; Convert ASCII to Integer
+    MOV AH, 0
+    MOV ROWS, AX
+
+    MOV AH, 09H
+    LEA DX, NEWLINE
+    INT 21H
+
+    ; Outer Loop (Rows) [cite: 9]
+    MOV CX, ROWS
+OUTER_LOOP:
+    PUSH CX         ; Save outer loop counter
+    
+    ; Inner Loop (Stars) [cite: 11]
+INNER_LOOP:
+    MOV AH, 02H
+    MOV DL, '*'     ; Print star [cite: 15]
+    INT 21H
+    LOOP INNER_LOOP
+
+    ; Print Newline after each row
+    MOV AH, 09H
+    LEA DX, NEWLINE
+    INT 21H
+
+    POP CX          ; Restore outer loop counter
+    LOOP OUTER_LOOP
+
+    ; Exit
+    MOV AH, 4CH
+    INT 21H
+MAIN ENDP
+END MAIN
