4541df16776ea46fb35ca0686678edf468ef4a1c
Arannamoy Mondal <arannamoymondal13@gmail.com>
2026-02-22 19:03:02 +0600

    upd

--- 01-variable.asm
+++ 01-variable.asm
@@ -12,6 +12,21 @@ main proc
     add msg,48
     mov dl,msg
     int 21h
+
+    mov ah,1
+    int 21h
+    mov msg1,al
+
+    mov ah,2
+    mov dl,10
+    int 21h
+    mov dl,13
+    int 21h
+
+    mov ah,2
+    mov dl,msg1
+    int 21h
+
     exit:
     mov ah,4ch
     int 21h
