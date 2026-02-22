5256c225672b89fde7e53e7b1aa696ccd54616b8
Arannamoy Mondal <arannamoymondal13@gmail.com>
2026-02-22 20:46:43 +0600

    upd

---
+++ 04-add.asm
@@ -0,0 +1,20 @@
+.model small
+.stack 100h
+.data
+msg0 db 'Variables: num1=$'
+msg1 db ', num2=$'
+msg2 db 'Output: Sum=$'
+
+
+.code
+main proc
+    mov ax,@data
+    mov ds,ax
+
+    mov ah,9
+    lea dx,msg0
+    int 21h
+
+    exit:
+main endp
+end main
