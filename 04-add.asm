4b7740595f6df37bcce6fb9599975450572f6cbf
Arannamoy Mondal <arannamoymondal13@gmail.com>
2026-02-22 21:01:26 +0600

    upd

--- 04-add.asm
+++ 04-add.asm
@@ -54,3 +54,82 @@ main proc
     int 21h
 main endp
 end main
+
+
+
+
+
+
+
+
+.model small
+.stack 100h
+
+.data
+msg0 db 'Variables: num1=$'
+msg1 db ', num2=$'
+msg2 db 'Output: Sum=$'
+
+.code
+main proc
+    mov ax,@data
+    mov ds,ax
+
+    ; Print num1 message
+    mov ah,9
+    lea dx,msg0
+    int 21h
+
+    ; Read num1
+    mov ah,1
+    int 21h
+    sub al,30h
+    mov bl,al
+
+    ; Print num2 message
+    mov ah,9
+    lea dx,msg1
+    int 21h
+
+    ; Read num2
+    mov ah,1
+    int 21h
+    sub al,30h
+    mov bh,al
+
+    ; Addition
+    mov al,bl
+    add al,bh
+    add al,30h       ; convert to ASCII
+
+    ; -----------------------------
+    ; Print newline before Output
+    mov dl,10       ; Line Feed
+    mov ah,2
+    int 21h
+    mov dl,13       ; Carriage Return
+    int 21h
+
+    ; Print Output message
+    mov ah,9
+    lea dx,msg2
+    int 21h
+
+    ; Print sum (single char)
+    mov dl,al
+    mov ah,2
+    int 21h
+
+    ; Final newline
+    mov dl,10
+    mov ah,2
+    int 21h
+    mov dl,13
+    int 21h
+
+    ; Terminate program
+    mov ah,4Ch
+    int 21h
+
+main endp
+end main
