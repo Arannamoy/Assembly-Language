d4cc3597dc2433d209c8f4c3a73bbdb29071150e
Arannamoy Mondal <arannamoymondal13@gmail.com>
2026-02-22 22:17:52 +0600

    upd

--- 04-add.asm
+++ 04-add.asm
@@ -1,22 +1,36 @@
-.model small
-.stack 100h
+.model small              ; small memory model
+.stack 100h               ; 256 bytes for stack
 
-.data
-msg0 db 'Variables: num1 =$'
-msg1 db ',num2=$'
-msg2 db 'Output: Result =$'
+.data                    
+msg1 db 3                ; msg1 with store 3
+msg2 db 5                ; msg1 with store 5
 
-
-.code
-main proc:
-    mov ax,@data
-    mov ds,ax
-
-    mov ah,9
-    lea dx,msg0
-    int 21h
-
+msg3 db 10,13,'Result: $' ; msg3 show result
     
+.code                     
 
-main endp
-end main
+main proc                 
+    
+    mov ax, @data         ; load address of data segment into ax
+    mov ds, ax            ; load ax into ds
+    
+    mov ah, 9             ; print string
+    lea dx, msg3          ; msg3 load into dx
+    int 21h               ; dos interrupt
+    
+    mov al, msg1          ; value of msg1 into al
+    mov bl, msg2          ;value of msg2 into bl
+     
+    add al, bl            ; subtruction bl from al. msg1 + msg2
+    
+    add al, 30h           ; convert into ascii code
+    
+    mov dl, al            ; load al into dl
+    mov ah, 2             ; print character
+    int 21h               ; dos interrupt
+    
+    mov ah, 4ch           ; terminate program
+    int 21h               ; dos interrupt
+    
+main endp  ;end main procedure      
+end main   ;end main execution                   
--- 05-Subtraction.asm
+++ 05-Subtraction.asm
@@ -2,9 +2,9 @@
 .stack 100h               ; 256 bytes for stack
 
 .data                    
-msg1 db 5                ; msg1 with store 3
-msg2 db 3                ; msg2 with store 5
-msg3 db 10,13,'Result: $' ; msg3 to show result
+msg1 db 9                ; msg1 with store 3
+msg2 db 4                ; msg2 with store 5
+msg3 db 10,13,'Result: $' ; msg3 show result
     
 .code                     
 
@@ -17,8 +17,8 @@ main proc
     lea dx, msg3          ; msg3 load into dx
     int 21h               ; dos interrupt
     
-    mov al, msg1             ; Move value of a into al 
-    mov bl, msg2             ; Move value of a into bl
+    mov al, msg1          ; value of msg1 into al 
+    mov bl, msg2          ; value of msg2 into bl
      
     sub al, bl            ; subtruction bl from al. msg1 - msg2
     
@@ -28,7 +28,7 @@ main proc
     mov ah, 2             ; print character
     int 21h               ; dos interrupt
     exit:
-        mov ah, 4ch           ; terminate program
+        mov ah, 4ch       ; terminate program
         int 21h               
     
 main endp ;end main procedure           
