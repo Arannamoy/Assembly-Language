0ace25c95866205cea9c68f6e65fe61f8e13a650
Arannamoy Mondal <arannamoymondal13@gmail.com>
2026-02-22 21:08:19 +0600

    upd

--- 04-add.asm
+++ 04-add.asm
@@ -61,6 +61,7 @@ end main
 
 .model small
 .stack 100h
+
 .data
 msg0 db 'Variables: num1=$'
 msg1 db ', num2=$'
@@ -71,62 +72,60 @@ main proc
     mov ax,@data
     mov ds,ax
 
-    ; print num1
+    ; Print num1 message
     mov ah,9
     lea dx,msg0
     int 21h
 
-    ; input num1
+    ; Input num1
     mov ah,1
     int 21h
     sub al,30h
     mov bl,al
 
-    ; print num2
+    ; Print num2 message
     mov ah,9
     lea dx,msg1
     int 21h
 
-    ; input num2
+    ; Input num2
     mov ah,1
     int 21h
     sub al,30h
     mov bh,al
 
-    ; addition
+    ; Addition
     mov al,bl
     add al,bh
-    add al,30h       ; ASCII conversion
+    add al,30h       ; convert to ASCII
 
-    ; newline before Output
+    ; Print newline before output
     mov dl,10
     mov ah,2
     int 21h
     mov dl,13
     int 21h
 
-    ; print Output message
+    ; Print Output message
     mov ah,9
     lea dx,msg2
     int 21h
 
-    ; print sum
+    ; Print sum (single character) properly
     mov dl,al
     mov ah,2
     int 21h
 
-    ; final newline
+    ; Final newline
     mov dl,10
     mov ah,2
     int 21h
     mov dl,13
     int 21h
 
-    ; terminate
+    ; Terminate program
     mov ah,4Ch
     int 21h
 
 main endp
 end main
-
-
